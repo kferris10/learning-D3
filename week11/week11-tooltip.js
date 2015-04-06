431b390ae32e740407366acf8f607c043a786f17
kferris10 <kevin.ferris10@gmail.com>
2015-04-06 15:30:44 -0600

    read in csv data

--- week11/week11-tooltip.js
+++ week11/week11-tooltip.js
@@ -11,9 +11,7 @@ var svg11 = d3.select("body").select("#week11-tooltip")
 	.attr("height", h);
 
 // data
-var tooltip_data = d3.csv("week11/tooltip-data.csv")
-    .row(function(d) { return {x: +d.X, y: +d.Y}; })
-    .get(function(error, rows) { console.log(rows); });
+var tooltip_data = d3.csv("week11/tooltip-data.csv");
 /*
 
 // scales
