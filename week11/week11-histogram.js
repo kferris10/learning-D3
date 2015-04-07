ccf4206314cd0713f6ea4ef860e3a754f807a626
kferris10 <kevin.ferris10@gmail.com>
2015-04-06 23:50:35 -0600

    Make a rough histogram

--- week11/week11-histogram.js
+++ week11/week11-histogram.js
@@ -11,8 +11,30 @@ var svg = d3.select("body").select("#week11-histogram")
   	.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// scales
+var x = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, width]);
+
 // data
 var values = d3.range(1000).map(d3.random.normal());
 var data11_histogram = d3.layout.histogram()
-	.bins(xticks(20))
-	(values);
+    .bins(x.ticks(20))
+    (values);
+
+var y = d3.scale.linear()
+	.domain([0, d3.max(data11_histogram, function(d) { return d.y; }) ])
+	.range([height, 0]);
+
+// draw bars
+var bar = svg.selectAll(".bar")
+    .data(data11_histogram)
+	.enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+bar.append("rect")
+    .attr("x", 1)
+    .attr("width", x(data11_histogram[0].dx) - 1)
+    .attr("height", function(d) { return height - y(d.y); });
+
